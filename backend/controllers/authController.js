c014073adeaa3dae18134580e6b68bd4661130dd
Astitwa Rathaur <astitwarathaur639@gmail.com>
2026-03-26 17:01:52 +0530

    Enhance security for cookie settings in authController
    Updated cookie settings to secure and sameSite: 'none' for better security in signUp, login, googleSignup functions.

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -40,8 +40,8 @@ export const signUp = async (req, res) => {
     // generate cookie
     res.cookie("token", token, {
       httpOnly: true,
-      secure: false,
-      sameSite: "Strict",
+      secure: true,
+      sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
     return res.status(201).json(user);
@@ -72,8 +72,8 @@ export const login = async (req, res) => {
     let token = await genToken(user._id);
     res.cookie("token", token, {
       httpOnly: true,
-      secure: false,
-      sameSite: "Strict",
+      secure: true,
+      sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
     return res.status(200).json(user);
@@ -108,8 +108,8 @@ export const googleSignup = async (req, res) => {
     let token = await genToken(user._id);
     res.cookie("token", token, {
       httpOnly: true,
-      secure: false,
-      sameSite: "Strict",
+      secure: true,
+      sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
     return res.status(200).json(user);
